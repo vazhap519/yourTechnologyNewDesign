d6a5b43484789be563216b659fbcbc78bff5a574
vazha pankvelashvili <phankvelashvili.mail@gmail.com>
2022-04-08 22:10:27 +0400

    add jquery & effects

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+$(".user_ico").click(function(e){
+    e.preventDefault
+    $(".regLog_user_form").show(1000)
+})
+
+$('.search_btn').click(function(){
+    $('.searhBox').show(1000)
+})
+});
