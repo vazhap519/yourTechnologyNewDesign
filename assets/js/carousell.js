60ca0804fd983ca6beb09adb64fbf507aa49e592
vazha pankvelashvili <phankvelashvili.mail@gmail.com>
2022-04-17 19:01:35 +0400

    start shop

--- assets/js/carousell.js
+++ assets/js/carousell.js
@@ -4,12 +4,6 @@ var topCarousellLeftBtn=document.querySelector('.carousell_area_arrows_left');
 var topCarousellRighttn=document.querySelector('.carousell_area_arrows_right');
 var TopCarousellContent=document.querySelectorAll('.carousell_area_box ');
 var TopCarousellCounter=0;
-if(topCarousellLeftBtn==true){
-    topCarousellLeftBtn.addEventListener('click',topCarousellLeftBtn_f)
-}else if(topCarousellLeftBtn==false){
-    console.log('arali')
-}
-
 function TopCarousellContentItems(){
     for(var i=0;i<TopCarousellContent.length;i++){
         TopCarousellContent[i].classList.remove('carousell_area_box_active')
